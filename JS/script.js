d8b2b362e03cfedc9acc57d88c9090c9bca3b5d2
hrishi-singh <singh.123@iitj.ac.in>
2024-06-02 12:52:43 +0530

    bug Fixes

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,3 @@
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-
+// import aos from 'aos';
+// import 'aos/dist/aos.css';
 // init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
-});
-
